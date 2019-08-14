35ccf62a75e7bb392271c2b2b1622ac2e8a52e28
mrkale <libor.gabaj@gmail.com>
2019-08-14 12:34:30 +0200

    statfilter: Version update

--- gbj_pythonlib_sw/statfilter.py
+++ gbj_pythonlib_sw/statfilter.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Module for statistical filtering and smoothing."""
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __status__ = 'Beta'
 __author__ = 'Libor Gabaj'
 __copyright__ = 'Copyright 2018-2019, ' + __author__
